92121e77f512cebdd19cf657c8a6f64c99ad9749
Robert <robert@zeiger.club>
2021-08-22 03:11:14 +0200

    First P2P Handshake impl.

--- src/main/java/p2p/P2pService.java
+++ src/main/java/p2p/P2pService.java
@@ -16,6 +16,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.*;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.concurrent.ExecutionException;
 import java.util.logging.Level;
@@ -58,25 +59,40 @@ public class P2pService {
                 List<String> messagesList = Stream.of(buffer.toString().split("\n", -1))
                         .collect(Collectors.toList());
 
-                //remove empty entries
+                // remove empty entries
                 messagesList.removeAll(Arrays.asList("", null));
 
-                //remove first char in every line if char is "/"
+                // remove first char as it's not needed
+                messagesList = messagesList.stream().map(e -> e.substring(1)).collect(Collectors.toList());
+
+                // remove first char in every line if char is "/"
                 List<String> lines = new ArrayList<>();
-                for (String value : messagesList)
+                for (String value : messagesList) {
                     if (value.startsWith("/")) {
                         value = value.substring(1);
-                        lines.add(value);
                     }
+                    if (value.startsWith("\n")) {
+                        value = value.substring(1);
+                    }
+                    lines.add(value);
+                }
                 messagesList.clear();
                 messagesList.addAll(lines);
 
                 //save Strings in Map
                 Map<String, String> connectionParameterMap = new HashMap<>();
                 for (String value : messagesList) {
-                    value = value.substring(1);
-                    List<String> parameterList = Arrays.stream(value.split("/")).collect(Collectors.toList());
-                    connectionParameterMap.put(parameterList.get(0), parameterList.size() == 1 ? "" : parameterList.get(1));
+                    List<String> parameterList = new ArrayList<>();
+
+                    if (!value.contains("/") && !value.contains(":")) {
+                        connectionParameterMap.put(value, "");
+                    } else if (value.contains("/")) {
+                        parameterList = Arrays.stream(value.split("/")).collect(Collectors.toList());
+                        connectionParameterMap.put(parameterList.get(0), parameterList.get(1));
+                    } else if (value.contains(":")) {
+                        parameterList = Arrays.stream(value.split(":")).collect(Collectors.toList());
+                        connectionParameterMap.put(parameterList.get(0), parameterList.get(1));
+                    }
                 }
 
                 if (connectionParameterMap.containsKey("multistream")) {
@@ -87,16 +103,81 @@ public class P2pService {
                             "\u0007/noise\n");
                 }
 
-                if (connectionParameterMap.containsKey("select"))
+                if (connectionParameterMap.containsKey("select")) {
                     LOGGER.info("Received select package from " + socket.remoteAddress());
+                    System.out.println("Sender ID: " + connectionParameterMap.get("select"));
+                    long ownId = 1L;
+                    String response = String.format("select:%s\n", ownId);
+
+                    // Byte conversion to make peer happy
+                    byte[] bytesFromDatabase = response.getBytes(StandardCharsets.UTF_8);
+                    byte[] responseAsBytes = new byte[1];
+                    byte[] combined = new byte[responseAsBytes.length + bytesFromDatabase.length];
+                    responseAsBytes[0] = (byte) bytesFromDatabase.length;
+                    System.arraycopy(responseAsBytes, 0, combined, 0, responseAsBytes.length);
+                    System.arraycopy(bytesFromDatabase, 0, combined, responseAsBytes.length, bytesFromDatabase.length);
+                    String responseAsString = new String(combined, StandardCharsets.UTF_8);
+
+                    //socket.write(Arrays.toString(combined));
+                    System.out.println("Own ID sent: " + ownId);
+
+                    socket.write(responseAsString);
+
+
+                    /*
+                    // for testing
+                    byte[] bytesFromDatabase2 = String.format("select:%s\n", connectionParameterMap.get("select")).getBytes();
+                    byte[] responseAsBytes2 = new byte[1];
+                    byte[] combined2 = new byte[responseAsBytes2.length + bytesFromDatabase2.length];
+                    responseAsBytes2[0] = (byte) bytesFromDatabase2.length;
+                    System.arraycopy(responseAsBytes2, 0, combined2, 0, responseAsBytes2.length);
+                    System.arraycopy(bytesFromDatabase2, 0, combined2, responseAsBytes2.length, bytesFromDatabase2.length);
+
+                    System.out.println(Arrays.toString(buffer.getBytes()));
+                    System.out.println(Arrays.toString(combined2));
+
+                     */
+                }
+
+                // TODO
+                if (connectionParameterMap.containsKey("nitiator")) {
+                    LOGGER.info("Received initiator package from " + socket.remoteAddress());
+                    String responseAsString = "\nresponder\n";
+                    socket.write(responseAsString);
+                }
+
+                if (connectionParameterMap.containsKey("noise") && connectionParameterMap.size() == 1) {
+                    LOGGER.info("Received noise package from " + socket.remoteAddress());
+                    String response = "/noise\n";
+                    // Byte conversion to make peer happy
+                    byte[] bytesFromDatabase = response.getBytes(StandardCharsets.UTF_8);
+                    byte[] responseAsBytes = new byte[1];
+                    byte[] combined = new byte[responseAsBytes.length + bytesFromDatabase.length];
+                    responseAsBytes[0] = (byte) bytesFromDatabase.length;
+                    System.arraycopy(responseAsBytes, 0, combined, 0, responseAsBytes.length);
+                    System.arraycopy(bytesFromDatabase, 0, combined, responseAsBytes.length, bytesFromDatabase.length);
+                    String responseAsString = new String(combined, StandardCharsets.UTF_8);
+
+                    socket.write(responseAsString);
+                }
+
+                /*
+                String test = "\u0000 \fS+�(��O�d\f!&�l��~ֳ��پ<���\u007F\n" +
+                        "�%"
+
+                  S+�(��O�d!&�l��~ֳ��پ<���
+�%
+                 */
 
             });
         });
 
         server.listen(res -> {
-            if (res.succeeded()) LOGGER.info("Vertx TCP server is now listening on port: " + server.actualPort());
-            else
+            if (res.succeeded()) {
+                LOGGER.info("Vertx TCP server is now listening on port: " + server.actualPort());
+            } else {
                 LOGGER.info("Vertx TCP server failed to bind!");
+            }
         });
     }
 
@@ -169,8 +250,9 @@ public class P2pService {
                 final Enumeration<?> e2 = ni.getInetAddresses();
                 while (e2.hasMoreElements()) {
                     inetAddress = (InetAddress) e2.nextElement();
-                    if (!inetAddress.isLoopbackAddress() && !inetAddress.getHostAddress().contains(":"))
+                    if (!inetAddress.isLoopbackAddress() && !inetAddress.getHostAddress().contains(":")) {
                         return inetAddress;
+                    }
                 }
             }
         } catch (final SocketException e) {
